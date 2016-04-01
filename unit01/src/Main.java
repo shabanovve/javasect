c592d931b09800c1888ba511dcf08d77f51e7c93
Vladimir Shabanov <vladimir.shabanov@dz.ru>
2016-04-01 23:24:52 +0400

    Add multiplicand loop

--- unit01/src/Main.java
+++ unit01/src/Main.java
@@ -2,29 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("===========");
-        new Column(2).show();
-
-        System.out.println("===========");
-        new Column(3).show();
-
-        System.out.println("===========");
-        new Column(4).show();
-
-        System.out.println("===========");
-        new Column(5).show();
-
-        System.out.println("===========");
-        new Column(6).show();
-
-        System.out.println("===========");
-        new Column(7).show();
-
-        System.out.println("===========");
-        new Column(8).show();
-
-        System.out.println("===========");
-        new Column(9).show();
+        for (int multiplicand = 2; multiplicand < 10; multiplicand ++){
+            System.out.println("===========");
+            new Column(multiplicand).show();
+        }
 
     }
 }
