7c672d2b01cfce2863adfdb3bc506fc1f83bec31
Vladimir Shabanov <vladimir.shabanov@dz.ru>
2016-04-01 23:10:56 +0400

    Add method for sufix and prefix

--- unit01/src/Column.java
+++ unit01/src/Column.java
@@ -7,19 +7,29 @@ public class Column {
         String prefix = "";
         String sufix = "";
         for (int i = 1; i <= 10; i++) {
-            if (i < 10) {
-                prefix = "2 x  ";
-            } else {
-                prefix = "2 x ";
-            }
-
-            if (2 * i < 10) {
-                sufix = " " + Integer.toString(2 * i);
-            } else {
-                sufix = Integer.toString(2 * i);
-            }
-
+            prefix = getPrefix(i);
+            sufix = getSufix(i);
             System.out.println(prefix + i + " = " + sufix);
         }
+    }
+
+    private String getSufix(int i) {
+        String sufix;
+        if (2 * i < 10) {
+            sufix = " " + Integer.toString(2 * i);
+        } else {
+            sufix = Integer.toString(2 * i);
+        }
+        return sufix;
+    }
+
+    private String getPrefix(int i) {
+        String prefix;
+        if (i < 10) {
+            prefix = "2 x  ";
+        } else {
+            prefix = "2 x ";
+        }
+        return prefix;
     }
 }
