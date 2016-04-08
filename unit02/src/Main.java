faad900c9e3196dc43f0a143e487ee8ffc9ec12b
Vladimir Shabanov <vladimir.shabanov@dz.ru>
2016-04-09 01:55:41 +0400

    #unit02 Remove old matrices

--- unit02/src/Main.java
+++ unit02/src/Main.java
@@ -1,9 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        int[][] firstMatrix = {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
-        int[][] secondMatrix = {{3, 3, 3}, {2, 2, 2}, {1, 1, 1}};
-        int[][] result;
 
         outputMatrix(firstMatrix, "First matrix");
         outputMatrix(secondMatrix, "Second matrix");
