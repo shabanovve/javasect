e594530576fc97b6cc51278d61912aea9edf838a
Vladimir Shabanov <vladimir.shabanov@dz.ru>
2016-04-09 01:23:47 +0400

    #unit02 Fill matrix with random numbers

--- unit02/src/Matrix.java
+++ unit02/src/Matrix.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * Created by Vladimir Shabanov on 09/04/16.
  */
@@ -7,10 +9,12 @@ public class Matrix {
     private int[][] elements = new int[DIMENTION][DIMENTION];
 
     public void prepare() {
+        Random randomGenerator = new Random();
         for (int i = 0; i < DIMENTION; i++) {
             for (int j = 0; j < DIMENTION; j++) {
-
+                elements[i][j] = randomGenerator.nextInt(10);
             }
         }
     }
+
 }
