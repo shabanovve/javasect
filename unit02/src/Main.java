359ea06c7c0a41796c5a38afdbfc66dfa0ec58c0
Vladimir Shabanov <vladimir.shabanov@dz.ru>
2016-04-09 02:34:52 +0400

    #unit02 Remove old matrices

--- unit02/src/Main.java
+++ unit02/src/Main.java
@@ -2,17 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Matrix a = new Matrix();
-        a.prepare();
-        a.outputMatrix("Matrix A with random numbers");
-
-        Matrix b = new Matrix();
-        b.prepare();
-        b.outputMatrix("Matrix B with random numbers");
-
-        Matrix c = Matrix.summarizeMatrices(a, b);
-        c.outputMatrix("Summarize two random matrix");
-
         Matrix[] matrices = new Matrix[10];
         for (int i = 0; i < 10; i++) {
             Matrix m = new Matrix();
