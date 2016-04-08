d1a72b97840652ffee1ba9b6ccfa4dbee64b5ae4
Vladimir Shabanov <vladimir.shabanov@dz.ru>
2016-04-09 00:09:18 +0400

    #unit02 Summarize matrices

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,27 @@ public class Main {
         int[][] secondMatrix = {{3, 3, 3}, {2, 2, 2}, {1, 1, 1}};
         int[][] result = new int[3][3];
 
-        outputMatrix(firstMatrix,"First matrix");
-        outputMatrix(secondMatrix,"Second matrix");
+        outputMatrix(firstMatrix, "First matrix");
+        outputMatrix(secondMatrix, "Second matrix");
 
+        summarizeMatrices(firstMatrix, secondMatrix, result);
+
+        outputMatrix(result,"Result matrix");
+
+    }
+
+    private static void summarizeMatrices(int[][] firstMatrix, int[][] secondMatrix, int[][] result) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                result[i][j] = firstMatrix[i][j] + secondMatrix[i][j];
+            }
+        }
     }
 
     private static void outputMatrix(int[][] firstMatrix, String title) {
         System.out.println(title);
         for (int i = 0; i < 3; i++) {
-            for (int j=0;j<3;j++){
+            for (int j = 0; j < 3; j++) {
                 System.out.print(firstMatrix[i][j] + " ");
             }
             System.out.println();
