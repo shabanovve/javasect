93372b6f3d323b334f3f3e50ca6dfa27f5349830
Vladimir Shabanov <vladimir.shabanov@dz.ru>
2016-04-09 00:27:54 +0400

    #unit02 Create Matrix class

---
+++ unit02/src/Matrix.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Vladimir Shabanov on 09/04/16.
+ */
+public class Matrix {
+
+    private static final int DIMENTION = 3;
+    private int[][] elements = new int[DIMENTION][DIMENTION];
+
+    public void prepare() {
+        for (int i = 0; i < DIMENTION; i++) {
+            for (int j = 0; j < DIMENTION; j++) {
+
+            }
+        }
+    }
+}
