7431bf6770e5d06b02b64477c219c6cc081a7ead
Vladimir Shabanov <vladimir.shabanov@dz.ru>
2016-04-08 23:55:37 +0400

    #unit02 Output matrix

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,17 @@
 public class Main {
 
     public static void main(String[] args) {
-        int[][] firstMatrix = {{1,1,1},{2,2,2},{3,3,3}};
-        int[][] secondMatrix = {{3,3,3},{2,2,2},{1,1,1}};
+        int[][] firstMatrix = {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
+        int[][] secondMatrix = {{3, 3, 3}, {2, 2, 2}, {1, 1, 1}};
 
+        System.out.println("First matrix");
+        for (int i = 0; i < 3; i++) {
+            for (int j=0;j<3;j++){
+                System.out.print(firstMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("=============");
 
     }
 }
